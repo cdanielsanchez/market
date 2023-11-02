a320d07f233e231e34f01c2529966f82c2e1e06d
cdanielsanchez <cdanielsanchez@gmail.com>
2023-11-02 08:32:32 -0500

    revision 7 de 35. aprendiendo spring boot

--- src/main/java/com/carlosd/market/HolaMundoController.java
+++ src/main/java/com/carlosd/market/HolaMundoController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// controller de ensayo 
+
 @RestController
 @RequestMapping("/saludar")
 public class HolaMundoController {
