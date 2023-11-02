ad22fbb40ba15fd82f641ce89dac01b1e4bc5c64
cdanielsanchez <cdanielsanchez@gmail.com>
2023-11-02 12:19:14 -0500

    revision 10 de 35, tenemos organizada la estructura del projecto

--- src/main/java/com/carlosd/market/web/controller/HolaMundoController.java
+++ src/main/java/com/carlosd/market/web/controller/HolaMundoController.java
@@ -1,11 +1,11 @@
-package com.carlosd.market;
+package com.carlosd.market.web.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// controller de ensayo 
+// controller de ensayo
 
 @RestController
 @RequestMapping("/saludar")
